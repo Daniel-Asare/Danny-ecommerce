3f355b4bbab9337a3f2409718c1a399cc7f55d07
Daniel-Asare <johndoe@example.com>
2021-07-25 14:26:18 +0000

    k

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -9,7 +9,7 @@ const Homepage = ()=> {
     <h1>AWESOME 
      SNEAKERS</h1>
      <p>Learn,hang,move.Whatever this school years throw your way nail it in style</p>
-    <Link to="/product">View Detail <span className="detail-icon"><BsArrowRight/></span></Link>
+    <Link to="/product">View Details <span className="detail-icon"><BsArrowRight/></span></Link>
   </div>
   )
 }
