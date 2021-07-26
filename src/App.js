225afff856206538edf6e546a8d2f3ec63f3aa54
Daniel-Asare <johndoe@example.com>
2021-07-26 17:35:56 +0000

    k

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import {BrowserRouter as Router,Route, Switch} from "react-router-dom";
 import {AppProvider} from "components/pages/Context";
 import {CartProvider} from 'react-use-cart'
 import Homepage from "components/pages/Homepage";
+import Products from "components/pages/Products";
+import Detail from "components/pages/Detail";
+import Cart from "components/pages/Cart";
+import Contact from "components/pages/Contact";
 
 
 function App() {
   return (
    <CartProvider>
     <AppProvider>
-      <Router>
-      
+      <Router basename='/'>
       <Header/>
-      <Section/>
+      <Route path="/" exact component={Homepage}/>
+      <Route path='/product' component={Products}/>
+      <Route path='/product/:id'  component={Detail}/>
+      <Route path="/cart" component={Cart}/>
+      <Route path='/contact'  component={Contact}/>
+      {/* <Section/> */}
       </Router>
     </AppProvider>
    </CartProvider>
